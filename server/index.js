2e60da3578148620d4299e7e5e7181dc4d2ec66a
AnandRP2030 <anand.rp2030@gmail.com>
2023-10-07 11:19:17 +0530

    Created MVC Folder strucute on the server side

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.get('/',(req, res) => {
+    res.send('check');
+})
+
+app.listen(3000, () => {
+    console.log(`Listening on port ${PORT}`);
+})
