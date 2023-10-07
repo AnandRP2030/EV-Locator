fc0efd1979b49e886207b26ed5be2b42947650c2
AnandRP2030 <anand.rp2030@gmail.com>
2023-10-07 12:51:15 +0530

    DB connected with server

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -2,32 +2,75 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const UserModel = require("../Models/user.model.js");
 const router = express.Router();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
   try {
     const { name, email, location, contact, password } = req.body;
 
-    
     if (!name || !email || !location || !contact || !password) {
-        return res.status(400).send("All fields are required");
+      return res.status(400).send("All fields are required");
     }
     if (email) {
-        const existUser = await UserModel.findOne({email});
-        if (existUser) {
-            return res.status(400).json ({message: "Email already used"});
-        }
+      const existUser = await UserModel.findOne({ email });
+      if (existUser) {
+        return res.status(400).json({ message: "Email already used" });
+      }
     }
-    bcrypt.hash(password, 5, (err, encryptedPassword) => {
-      UserModel.create({
-        name,
-        email,
-        location,
-        contact,
-        password: encryptedPassword,
-      }).then(() => {
-        res.status(201).send("Registration Completed");
-      });
+
+    // hash the password
+    const saltRounds = 10;
+    const hashPassword = await bcrypt.hash(password, saltRounds);
+    const newUser = new UserModel({
+      name,
+      email,
+      location,
+      contact,
+      password: hashPassword,
     });
+    await newUser.save();
+    return res.status(201).json({ message: "User Registered Successfully" });
+  } catch (error) {
+    res.status(500).send(error, "Server Error");
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(404).send({ error: "Please fill all fields" });
+    }
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(404)
+        .send({ message: "User or password is incorrect." });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res
+        .status(401)
+        .send({ message: "Username or password is incorrect." });
+    }
+
+    // geneate token
+    const token = jwt.sign(
+      {
+        userId: user._id,
+        name: user.name,
+        email,
+        location: user.location,
+        contact: user.contact,
+      },
+      JWT_SECRET_KEY,
+      { expiresIn: "100h" }
+    );
+
+    return res.status(200).send({ message: "Login success.", user, token });
   } catch (error) {
     res.status(500).send(error, "Server Error");
   }
--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
-const express = require('express');
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
 const PORT = process.env.PORT || 3000;
+const MONGO_DB_URL = process.env.MONGO_DB_URL;
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.json());
 
-app.get('/',(req, res) => {
-    res.send('check');
-})
+app.get("/", (req, res) => {
+  res.send("check");
+});
 
-app.listen(3000, () => {
-    console.log(`Listening on port ${PORT}`);
-})
+mongoose
+  .connect(MONGO_DB_URL)
+  .then(() => {
+    console.log("DB Connected.");
+    app.listen(3000, () => {
+      console.log(`Listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
