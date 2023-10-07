ac5d242d6577d501ae1ca333b43502bf7553306f
AnandRP2030 <anand.rp2030@gmail.com>
2023-10-07 12:51:41 +0530

    login and sign up page added

--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -1,9 +1,10 @@
+import Container from 'react-bootstrap/Container';
+
 const Signup = () => {
-    return (
-      <>
-        <h1> Signup </h1>
-      </>
-    );
-  };
-  export default Signup;
-  
+  return (
+    <Container fluid>
+      <h1> Signup </h1>
+    </Container>
+  );
+};
+export default Signup;
